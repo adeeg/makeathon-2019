74be184eb9c07ddbf6ecfff5abc775c3ceca98a7
danjones1618 <bluedragoncubed@gmail.com>
2019-02-03 12:14:18 +0000

    fix

--- ser.py
+++ ser.py
@@ -4,7 +4,7 @@ from threading import Thread
 keyDown = None
 
 def thread1(threadname):
-    s = serial.Serial("/dev/ttyUSB0", 1200, timeout=5)
+    s = serial.Serial("/dev/ttyUSB1", 1200, timeout=5)
     while True:
         global keyDown
         keyDown = s.readline()
